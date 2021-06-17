49c2701cedf2cfb401fcb8af3c0faa69e7d8e438
Ophy Boamah <ophybee8@gmail.com>
2021-06-17 12:15:36 +0000

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Nav/>
   <router-view/>
 </template>
+
+<script>
+import Nav from '@/components/Nav.vue';
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
 
 <style>
 #app {
